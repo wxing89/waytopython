212f6a9e3681cda2de5244cc4ae1d9c0643ffdbe
roose <roo@ubuntu.(none)>
2014-06-27 17:45:53 +0800

    if

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,7 +3,15 @@
 
 # Fibonacci series:
 # the sum of two elements defines the text
-a, b = 0, 1
-while b < 1000:
-    print b,
-    a, b = b, a+b
+
+# write Fibonacci series up to n
+def fib(n):
+    '''Print a Fibonacci series up to n.'''
+    a, b = 0, 1
+    while a < n:
+        print a,
+        a, b = b, a+b
+
+# Now call the function we just defined:
+fib(2000)
+
